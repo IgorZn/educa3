63b1766f813908939107f070cd3617cf3575b5c2
Igor Znamensky <igor.znamensky@gmail.com>
2020-11-18 22:23:53 +0300

    10 - Building an E-Learning Platform Creating class-based views Next: Using mixins for class-based views

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Course
 
 # Create your views here.
+
+"""
+This is the ManageCourseListView view. It inherits from Django's generic ListView.
+You override the get_queryset() method of the view to retrieve only courses
+created by the current user. To prevent users from editing, updating, or deleting
+courses they didn't create, you will also need to override the get_queryset()
+method in the create, update, and delete views. When you need to provide a specific
+behavior for several class-based views, it is recommended that you use mixins.
+"""
+
+
+class ManageCourseListView(ListView):
+	model = Course
+	template_name = 'courses/manage/course/list.html'
+
+	def get_queryset(self):
+		qs = super().get_queryset()
+		return qs.filter(owner=self.request.user)
